5258386759476176aac3e5c5499d549ce70f85ea
Zack Proser <zackproser@gmail.com>
2018-01-14 13:08:17 -0800

    Example of building a mathematical expression using a tree

--- Trees/index.js
+++ Trees/index.js
@@ -14,3 +14,21 @@ console.log(root.right.val) //c
 
 root.right.val = 'Greetings'
 console.log(root.right.val) //Greetings
+
+/**
+ * Define a simple mathematical expression using a tree,
+ * whereby the root defines the operation and the leaves define the values
+ *
+ *    ( * )
+ *    /   \
+ *   /     \
+ * ( 9 )  ( 9 )
+ *
+ */
+let mathRoot = new Node('*')
+mathRoot.insertLeft(new Node(9))
+mathRoot.insertRight(new Node(9))
+
+let expression = `${mathRoot.left.val} ${mathRoot.val} ${mathRoot.right.val}`
+
+console.log(`Parsed expression: ${eval(expression)}`) // 81
