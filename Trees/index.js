a677eb5869f4b9dde603f7cb39b42ce04930d50c
Zack Proser <zackproser@gmail.com>
2018-01-14 14:36:49 -0800

    Implement function for building a parse tree

--- Trees/index.js
+++ Trees/index.js
@@ -32,3 +32,69 @@ mathRoot.insertRight(new Node(9))
 let expression = `${mathRoot.left.val} ${mathRoot.val} ${mathRoot.right.val}`
 
 console.log(`Parsed expression: ${eval(expression)}`) // 81
+
+
+/**
+ * Parse a more complex mathematical expression using a tree:
+ *
+ * (3 + (4 * 5))
+ *
+ *      ( + )
+ *     /    \
+ *    /      \
+ * ( 3 )    ( * )
+ *         /     \
+ *        /       \
+ *     ( 4 )     ( 5 )
+ */
+
+let buildParseTree = (exp) => {
+  //Remove all whitespace from expression
+  exp = exp.replace(/\s/g, "")
+  var
+    operators = ['+', '-', '*', '/']
+    leftParend = '('
+    rightParend = ')'
+    tree = {}
+    stack = [tree]
+    node = tree
+    //Iterate through the tokens in the expression
+    for (var i=0; i<exp.length; i++) {
+      const token = exp[i]
+      if (token === leftParend) {
+        //We have a new expression, so start a new tree
+        node.left = {}
+        stack.push(node)
+        node = node.left
+      } else if (token === rightParend) {
+        //We've reached the end of an expression
+        node = stack.pop()
+      } else if (operators.indexOf(token) > -1) {
+        //We have an operator, so set the current node to the operator,
+        //then create and advance to a new right child
+        node.val = token
+        node.right = {}
+        stack.push(node)
+        node = node.right
+      } else {
+        //We have an operand (numerical value), set the current node value to the operand,
+        //and use the stack to retreat back to the current node's parent
+        node.val = Number(token)
+        parent = stack.pop()
+        node = parent
+      }
+    }
+    return tree
+}
+
+console.log(buildParseTree('(3 + (4 * 5))'))
+/**
+ *
+{ left: { val: 3 },
+  val: '+',
+  right: { left: { val: 4 }, val: '*', right: { val: 5 } } }
+ */
+
+
+
+
